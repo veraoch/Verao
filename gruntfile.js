ac08b08f220a64fabb1abbabc7f8c1219a521165
BenSibley <ben@competethemes.com>
2016-03-10 14:18:47 -0500

    lots of stuff...

--- gruntfile.js
+++ gruntfile.js
@@ -15,6 +15,7 @@ module.exports = function(grunt) {
             dist: {
                 files: {
                     'js/build/production.min.js' : 'js/build/production.js',
+                    'js/build/customizer.min.js' : 'js/customizer.js',
                     'js/build/postMessage.min.js' : 'js/postMessage.js'
                 }
             }
