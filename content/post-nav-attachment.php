ec892c3889b5751c1ead7ec554dbefab4967b062
BenSibley <ben@competethemes.com>
2016-04-08 14:27:50 -0400

    attachment pages now use full size images, captions, and descriptions

--- content/post-nav-attachment.php
+++ content/post-nav-attachment.php
@@ -1,8 +1,8 @@
 <nav class="further-reading">
 	<div class="previous">
-		<span><?php previous_image_link( false, __( 'Previous Image', 'cele' ) ); ?></span>
+		<?php previous_image_link( false, __( 'Previous Image', 'cele' ) ); ?>
 	</div>
 	<div class="next">
-		<span><?php next_image_link( false, __( 'Next Image', 'cele' ) ); ?></span>
+		<?php next_image_link( false, __( 'Next Image', 'cele' ) ); ?>
 	</div>
 </nav>
