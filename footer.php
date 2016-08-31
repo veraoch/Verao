1591a5ab739e52d7f62ec6ab6ffca7d5d19d93e8
BenSibley <ben@competethemes.com>
2016-08-31 09:44:54 -0400

    footer text updated

--- footer.php
+++ footer.php
@@ -8,7 +8,7 @@
     <div class="design-credit">
         <span>
             <?php
-            $footer_text = sprintf( __( '<a href="%s">Cele WordPress Theme</a> by Compete Themes.', 'cele' ), 'https://www.competethemes.com/cele/' );
+            $footer_text = sprintf( __( '<a href="%s">Cele Theme</a> by Compete Themes.', 'cele' ), 'https://www.competethemes.com/cele/' );
             $footer_text = apply_filters( 'ct_cele_footer_text', $footer_text );
             echo wp_kses_post( $footer_text );
             ?>
