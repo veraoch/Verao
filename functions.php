fc4f5daa9eb979ff8529733eb82d402f7413fdf5
BenSibley <ben@competethemes.com>
2016-12-01 18:58:00 -0500

    added support for custom CSS section in WP 4.7

--- functions.php
+++ functions.php
@@ -493,7 +493,11 @@ add_filter( 'post_class', 'ct_cele_post_class' );
 if ( ! function_exists( ( 'ct_cele_custom_css_output' ) ) ) {
 	function ct_cele_custom_css_output() {
 
-		$custom_css = get_theme_mod( 'custom_css' );
+		if ( function_exists( 'wp_get_custom_css' ) ) {
+			$custom_css = wp_get_custom_css();
+		} else {
+			$custom_css = get_theme_mod( 'custom_css' );
+		}
 
 		if ( ! empty( $custom_css ) ) {
 			$custom_css = ct_cele_sanitize_css( $custom_css );
