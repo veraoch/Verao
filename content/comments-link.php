63e6b103f11983c9170fac63cee676e384b514b8
BenSibley <ben@competethemes.com>
2017-02-28 13:43:24 -0500

    disambiguation for one more string

--- content/comments-link.php
+++ content/comments-link.php
@@ -2,10 +2,10 @@
 	<i class="fa fa-comment" title="<?php _e( 'comment icon', 'cele' ); ?>"></i>
 	<?php
 	if ( ! comments_open() && get_comments_number() < 1 ) :
-		comments_number( __( 'Comments closed', 'cele' ), __( '1 Comment', 'cele' ), __( '% Comments', 'cele' ) );
+		comments_number( __( 'Comments closed', 'cele' ), __( '1 Comment', 'cele' ), _x( '% Comments', 'noun: 5 comments', 'cele' ) );
 	else :
 		echo '<a href="' . esc_url( get_comments_link() ) . '">';
-		comments_number( __( 'Leave a Comment', 'cele' ), __( '1 Comment', 'cele' ), __( '% Comments', 'cele' ) );
+		comments_number( __( 'Leave a Comment', 'cele' ), __( '1 Comment', 'cele' ), _x( '% Comments', 'noun: 5 comments', 'cele' ) );
 		echo '</a>';
 	endif;
 	?>
