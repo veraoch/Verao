f92ed0df0d6819f1959e74cbc0bb6de09be2eb93
BenSibley <ben@competethemes.com>
2017-08-11 11:03:41 -0400

    no longer showing sticky status on search results

--- functions.php
+++ functions.php
@@ -416,7 +416,7 @@ add_filter( 'walker_nav_menu_start_el', 'ct_cele_nav_dropdown_buttons', 10, 4 );
 if ( ! function_exists( ( 'ct_cele_sticky_post_marker' ) ) ) {
 	function ct_cele_sticky_post_marker() {
 
-		if ( is_sticky() && ! is_archive() ) {
+		if ( is_sticky() && !is_archive() && !is_search() ) {
 			echo '<div class="sticky-status"><span>' . esc_html__( "Featured", "cele" ) . '</span></div>';
 		}
 	}
