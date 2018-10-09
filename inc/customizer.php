544754242ffbbb50fd5adeec23956559ad3a3588
BenSibley <ben@competethemes.com>
2018-10-09 16:53:58 -0400

    added new social media icons

--- inc/customizer.php
+++ inc/customizer.php
@@ -143,6 +143,10 @@ function ct_cele_add_customizer_content( $wp_customize ) {
 				$label = 'Tencent Weibo';
 			} elseif ( $social_site == 'paypal' ) {
 				$label = 'PayPal';
+			} elseif ( $social_site == 'stack-overflow' ) {
+				$label = 'Stack Overflow';
+			} elseif ( $social_site == 'ok-ru' ) {
+				$label = 'OK.ru';
 			} elseif ( $social_site == 'email-form' ) {
 				$label = 'Contact Form';
 			}
