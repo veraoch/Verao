93b7326ac803133b83d1cda28a2b157562b30d9e
BenSibley <ben@competethemes.com>
2019-02-20 11:14:12 -0500

    Cele Pro can now output shortcodes in the Footer Text Customizer section

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
             <?php
             $footer_text = sprintf( __( '<a href="%1$s">%2$s Theme</a> by Compete Themes.', 'cele' ), 'https://www.competethemes.com/cele/', wp_get_theme( get_template() ) );
             $footer_text = apply_filters( 'ct_cele_footer_text', $footer_text );
-            echo wp_kses_post( $footer_text );
+            echo do_shortcode( wp_kses_post( $footer_text ) );
             ?>
         </span>
     </div>
