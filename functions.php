8dd1e06b62b999b97ba54539f0d005a23299847b
BenSibley <ben@competethemes.com>
2019-11-27 09:36:39 -0500

    removed incorrect theme support function for full width images

--- functions.php
+++ functions.php
@@ -61,7 +61,6 @@ if ( ! function_exists( ( 'ct_cele_theme_setup' ) ) ) {
 
 		// Gutenberg - wide & full images
 		add_theme_support( 'align-wide' );
-		add_theme_support( 'align-full' );
 
 		// Gutenberg - add support for editor styles
 		add_theme_support('editor-styles');
