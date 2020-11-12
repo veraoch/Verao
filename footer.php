4beb09fd04591457acaef221a8522cbf6d9d6b65
Vera Pilloni <vera@MacBook-di-Vera-Pilloni.local>
2020-11-12 22:33:30 +0100

    ported style.css changes

--- footer.php
+++ footer.php
@@ -12,7 +12,7 @@ if ( ! function_exists( 'elementor_theme_do_location' ) || ! elementor_theme_do_
     <div class="design-credit">
         <span>
             <?php
-            $footer_text = sprintf( __( '<a href="%1$s">%2$s Theme</a> by Compete Themes.', 'cele' ), 'https://www.competethemes.com/cele/', wp_get_theme( get_template() ) );
+            $footer_text = sprintf( __( '<a href="%1$s">%2$s Theme</a>', 'verao' ), 'https://github.com/veraoch/Verao/', wp_get_theme( get_template() ) );
             $footer_text = apply_filters( 'ct_cele_footer_text', $footer_text );
             echo do_shortcode( wp_kses_post( $footer_text ) );
             ?>
